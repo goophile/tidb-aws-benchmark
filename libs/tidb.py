160a8782b219aad5b41178dc11037a9841f508b5
Albert <goophile@gmail.com>
2021-01-24 16:50:03 +0800

    Benchmark with go-tpc
    close #5

--- libs/tidb.py
+++ libs/tidb.py
@@ -15,6 +15,7 @@ class ControlServer:
 
         self.ssh = SSH(host)
         self.ssh.login(username, keyfile)
+        self.tidb_host = '10.0.1.11'  # keep the same as tidb-topology.yaml
 
     def _put_ssh_key(self):
         # All EC2 instances share the same key, so copy the private key to ControlServer
@@ -39,10 +40,44 @@ class ControlServer:
 
     def install_tidb(self, template_path: str, version: str):
         """
-        Install TiDB with TiUP.
+        Install TiDB with TiUP, and start TiDB cluster.
         """
         yaml = f'/home/{self.username}/tidb-template.yaml'
         SFTP(self.host, 22, self.username, self.keyfile).put(template_path, yaml)
 
         cmd = f"yes | tiup cluster deploy tidb-test {version} {yaml} --user {self.username}"
+        self.ssh.exec_cmd(cmd)
+
+        self.ssh.exec_cmd('tiup cluster start tidb-test')
+        self.ssh.exec_cmd('tiup cluster display tidb-test')
+
+    def install_go_tpc(self, version: str = '1.0.4'):
+        """
+        Download go-tpc from Github.
+        """
+        url = ('https://github.com/pingcap/go-tpc/releases/download/'
+               f'v{version}/go-tpc_{version}_linux_amd64.tar.gz')
+        self.ssh.exec_cmd(f'wget "{url}"')
+        self.ssh.exec_cmd('tar xzf go-tpc_1.0.4_linux_amd64.tar.gz')
+
+    def prepare_data(self, warehouses: int = 4, parts: int = 4):
+        """
+        Insert data into TiDB.
+        """
+        cmd = f'./go-tpc tpcc -H {self.tidb_host} --warehouses {warehouses} --parts {parts} prepare'
+        self.ssh.exec_cmd(cmd, timeout=300)
+
+    def clean_data(self, warehouses: int = 4):
+        """
+        Clean the prepared data.
+        """
+        cmd = f'./go-tpc tpcc -H {self.tidb_host} --warehouses {warehouses} cleanup'
+        self.ssh.exec_cmd(cmd, timeout=300)
+
+    def benchmark(self, threads: int = 25, time: int = 60, warehouses: int = 4):
+        """
+        Benchmark with go-tpc.
+        """
+        cmd = (f'./go-tpc tpcc -H {self.tidb_host} '
+               f'-T {threads} --time {time}s --warehouses {warehouses} run')
         self.ssh.exec_cmd(cmd)
--- main.py
+++ main.py
@@ -15,10 +15,19 @@ def main():
     parser.add_argument('--ec2_template', default=None, help='EC2 Cloudformation template')
     parser.add_argument('--delete_ec2', action='store_true', help='Delete Cloudformation stack')
 
-    parser.add_argument('--install_tidb', action='store_true', help='Install TiUP and TiDB')
+    parser.add_argument('--install_tidb', action='store_true', help='Install TiUP, TiDB, go-tpc')
     parser.add_argument('--tidb_version', default='4.0.10', help='TiDB version, default 4.0.10')
     parser.add_argument('--tidb_template', default=None,
                         help='TiDB topology template, should match Cloudformation template')
+
+    parser.add_argument('--prepare_data', action='store_true',
+                        help='Install go-tpc and insert data into TiDB')
+    parser.add_argument('--clean_data', action='store_true', help='Clean the benchmark data')
+    parser.add_argument('--benchmark', action='store_true', help='Benchmark with go-tpc')
+    parser.add_argument('--tpc_warehouses', default='4', help='go-tpc warehouses, default 4')
+    parser.add_argument('--tpc_parts', default='4', help='go-tpc partitions, default 4')
+    parser.add_argument('--tpc_time', default='60', help='go-tpc duration in seconds, default 60')
+    parser.add_argument('--tpc_threads', default='10', help='go-tpc threads, default 10')
 
     args = parser.parse_args()
 
@@ -46,14 +55,24 @@ def main():
     if args.delete_ec2:
         aws.delete_ec2_stack()
 
-    if args.install_tidb:
-        assert args.tidb_template
-
+    if args.install_tidb or args.prepare_data or args.clean_data or args.benchmark:
         ec2_ip = aws.get_ec2_ip()
         ctrl = ControlServer(ec2_ip, 'ubuntu', str(aws.private_key_path))
-        ctrl.install_tiup()
 
+    if args.install_tidb:
+        assert args.tidb_template
+        ctrl.install_tiup()
         ctrl.install_tidb(args.tidb_template, args.tidb_version)
+        ctrl.install_go_tpc()
+
+    if args.prepare_data:
+        ctrl.prepare_data(args.tpc_warehouses, args.tpc_parts)
+
+    if args.clean_data:
+        ctrl.clean_data(args.tpc_warehouses)
+
+    if args.benchmark:
+        ctrl.benchmark(args.tpc_threads, args.tpc_time, args.tpc_warehouses)
 
 
 if __name__ == "__main__":
